0a4543853f110370a3780472671bfb7a04710922
TANG ZHIXIONG <zhixiong@momenta.ai>
2022-10-22 19:27:33 +0800

    init playground

---
+++ tests/test_cheap_ruler.cpp
@@ -0,0 +1,14 @@
+#define DBG_MACRO_NO_WARNING
+#include <dbg.h>
+
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "doctest.h"
+
+#include "cpp_example/cheap_ruler.hpp"
+
+TEST_CASE("cheap ruler")
+{
+    auto ruler = mapbox::cheap_ruler::CheapRuler(
+        45.0, mapbox::cheap_ruler::CheapRuler::Unit::Meters);
+    dbg(ruler.k());
+}
