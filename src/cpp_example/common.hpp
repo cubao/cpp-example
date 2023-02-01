995b4e627aab4fd20d0936e32c3a60759c0eb50e
TANG ZHIXIONG <zhixiong.tang@momenta.ai>
2023-02-01 20:57:50 +0800

    add common, rapidjson_io

---
+++ src/cpp_example/common.hpp
@@ -0,0 +1,8 @@
+#ifndef CUBAO_COMMON_HPP
+#define CUBAO_COMMON_HPP
+
+// should sync
+// - https://github.com/cubao/cpp-example/blob/main/src/cpp_example/common.hpp
+// - https://github.com/cubao/headers/tree/main/include/cubao/common.hpp
+
+#endif
---
+++ src/cpp_example/rapidjson_io.hpp
@@ -0,0 +1,8 @@
+#ifndef CUBAO_RAPIDJSON_IO
+#define CUBAO_RAPIDJSON_IO
+
+// should sync
+// - https://github.com/cubao/cpp-example/blob/main/src/cpp_example/rapidjson_io.hpp
+// - https://github.com/cubao/headers/tree/main/include/cubao/rapidjson_io.hpp
+
+#endif
