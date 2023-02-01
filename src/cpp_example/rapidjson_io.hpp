d2b216ed448d6bf62c347bee6e35a73885f6ae8a
TANG ZHIXIONG <zhixiong.tang@momenta.ai>
2023-02-01 21:06:01 +0800

    not ready

--- src/cpp_example/rapidjson_io.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef CUBAO_RAPIDJSON_IO
-#define CUBAO_RAPIDJSON_IO
-
-// should sync
-// - https://github.com/cubao/cpp-example/blob/main/src/cpp_example/rapidjson_io.hpp
-// - https://github.com/cubao/headers/tree/main/include/cubao/rapidjson_io.hpp
-
-#endif
---
+++ tests/test_rapidjson_io.cpp
@@ -0,0 +1,14 @@
+#define DBG_MACRO_NO_WARNING
+#include <dbg.h>
+
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "doctest.h"
+
+#include "cpp_example/rapidjson_io.hpp"
+
+#include "cista/verify.h"
+
+TEST_CASE("from_to_rapidjson")
+{
+    //
+}
