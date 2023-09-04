225a3515fd25ca1100f25fb14bc8b0e30c283d49
TANG ZHIXIONG <zhixiong.tang@momenta.ai>
2023-09-04 23:56:54 +0800

    add dummy test

--- tests/test_sample.cpp
+++ tests/test_sample.cpp
@@ -6,4 +6,27 @@
 
 #include "cpp_example/packedrtree.h"
 
-TEST_CASE("dummy") {}
+TEST_CASE("dummy") {
+    using namespace FlatGeobuf;
+    auto nodes = std::vector<NodeItem>{
+        {0, 0, 1, 1},
+        {2, 2, 3, 3}
+    };
+    auto extent = calcExtent(nodes);
+    CHECK(nodes[0].intersects({0, 0, 1, 1}));
+    CHECK(nodes[1].intersects({2, 2, 3, 3}));
+
+    int index = 0;
+    for (auto &node: nodes) {
+        node.offset += index;
+        ++index;
+    }
+    hilbertSort(nodes);
+
+    auto rtree = PackedRTree(nodes, extent);
+    auto hits = rtree.search(0, 0, 1, 1);
+    for (auto &h: hits) {
+        dbg(h.index);
+        dbg(h.offset);
+    }
+}
