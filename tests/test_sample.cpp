b75f863912e0d613a814621150925f7d8461efe8
TANG ZHIXIONG <zhixiong.tang@momenta.ai>
2023-09-05 23:01:50 +0800

    works

--- tests/test_sample.cpp
+++ tests/test_sample.cpp
@@ -1,3 +1,5 @@
+#include "matplotlibcpp.h"
+
 #define DBG_MACRO_NO_WARNING
 #include <dbg.h>
 
@@ -5,9 +7,6 @@
 #include "doctest.h"
 
 #include <vector>
-#include <Eigen/Core>
-#include <Eigen/Dense>
-#include "matplotlibcpp.h"
 
 namespace plt = matplotlibcpp;
 
@@ -15,7 +14,7 @@ TEST_CASE("minimal")
 {
     std::vector<double> y = {1, 3, 2, 4};
     plt::plot(y);
-    plt::savefig("minimal.pdf");
+    plt::save("minimal.pdf");
 }
 
 TEST_CASE("standard")
@@ -38,26 +37,5 @@ TEST_CASE("standard")
     plt::title("Standard usage"); // set a title
     plt::legend();                // enable the legend
 
-    plt::savefig("standard.pdf"); // save the figure
-}
-
-TEST_CASE("eigen")
-{
-    Eigen::VectorXd x = Eigen::VectorXd::LinSpaced(200, 0, 6);
-    Eigen::VectorXd y, z;
-
-    // y = exp(sin(x)), z = exp(cos(z))
-    y = x.array().sin().exp().matrix();
-    z = x.array().cos().exp().matrix();
-
-    plt::figure();
-
-    plt::semilogx(x, y, "tab:red");
-    plt::semilogx(x, z, "tab:blue", {{"linestyle", "--"}});
-
-    plt::xlabel("Time in lecture");
-    plt::ylabel("Student confusion");
-
-    plt::grid();
-    plt::show(); // show the figure instead of saving it
+    plt::save("standard.pdf"); // save the figure
 }
