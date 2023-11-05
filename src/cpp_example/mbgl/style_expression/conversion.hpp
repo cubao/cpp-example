c6cc2c86f15602f8433f864d385ec8bc6bec577b
TANG ZHIXIONG <zhixiong.tang@momenta.ai>
2023-11-05 19:08:39 +0800

    lint code

--- src/cpp_example/mbgl/style_expression/conversion.hpp
+++ src/cpp_example/mbgl/style_expression/conversion.hpp
@@ -2,28 +2,31 @@
 
 #include <string>
 
-namespace mbgl {
-namespace style {
-namespace conversion {
+namespace mbgl
+{
+namespace style
+{
+namespace conversion
+{
 
-// This is a forward-declaration only header intended to minimize dependencies and to improve
-// compilation speed. In order to specialize implementations and get access to the actual
-// implementation, include <mbgl/style/conversion_impl.hpp>.
+// This is a forward-declaration only header intended to minimize dependencies
+// and to improve compilation speed. In order to specialize implementations and
+// get access to the actual implementation, include
+// <mbgl/style/conversion_impl.hpp>.
 
-struct Error { std::string message; };
+struct Error
+{
+    std::string message;
+};
 
-template <typename T>
-class ConversionTraits;
+template <typename T> class ConversionTraits;
 
 class Convertible;
 
-template <typename T, typename Enable = void>
-struct Converter;
+template <typename T, typename Enable = void> struct Converter;
 
-template <typename T, typename Enable = void>
-struct ValueFactory;
+template <typename T, typename Enable = void> struct ValueFactory;
 
 } // namespace conversion
 } // namespace style
 } // namespace mbgl
-
--- src/cpp_example/mbgl/style_expression/traits.hpp
+++ src/cpp_example/mbgl/style_expression/traits.hpp
@@ -5,34 +5,56 @@
 #include <type_traits>
 #include <vector>
 
-namespace mbgl {
+namespace mbgl
+{
 
-template<typename T>
-constexpr auto underlying_type(T t) -> typename std::underlying_type_t<T> {
+template <typename T>
+constexpr auto underlying_type(T t) -> typename std::underlying_type_t<T>
+{
     return typename std::underlying_type_t<T>(t);
 }
 
-template <typename T> struct is_utf16char_like: std::false_type {};
-template <typename T> struct is_utf16char_like<const T>: is_utf16char_like<T> {};
-template <> struct is_utf16char_like<char16_t>: std::true_type {};
-template <> struct is_utf16char_like<wchar_t>: std::true_type {};
-template <> struct is_utf16char_like<uint16_t>: std::true_type {};
+template <typename T> struct is_utf16char_like : std::false_type
+{
+};
+template <typename T> struct is_utf16char_like<const T> : is_utf16char_like<T>
+{
+};
+template <> struct is_utf16char_like<char16_t> : std::true_type
+{
+};
+template <> struct is_utf16char_like<wchar_t> : std::true_type
+{
+};
+template <> struct is_utf16char_like<uint16_t> : std::true_type
+{
+};
 
-template <typename T> using is_utf16char_like_pointer = std::integral_constant<bool, std::is_pointer<T>::value
-                                                                                     && is_utf16char_like<typename std::remove_pointer<T>::type>::value>;
+template <typename T>
+using is_utf16char_like_pointer = std::integral_constant<
+    bool, std::is_pointer<T>::value &&
+              is_utf16char_like<typename std::remove_pointer<T>::type>::value>;
 
 template <class OutPointer, class InChar>
-typename std::enable_if<is_utf16char_like<InChar>::value && is_utf16char_like_pointer<OutPointer>::value, OutPointer>::type utf16char_cast(InChar *in)
+typename std::enable_if<is_utf16char_like<InChar>::value &&
+                            is_utf16char_like_pointer<OutPointer>::value,
+                        OutPointer>::type
+utf16char_cast(InChar *in)
 {
     return reinterpret_cast<OutPointer>(in);
 }
 
-template <typename T>
-struct is_linear_container : std::false_type {};
+template <typename T> struct is_linear_container : std::false_type
+{
+};
 
 template <typename T, std::size_t N>
-struct is_linear_container<std::array<T, N>> : std::true_type {};
+struct is_linear_container<std::array<T, N>> : std::true_type
+{
+};
 template <typename... Ts>
-struct is_linear_container<std::vector<Ts...>> : std::true_type {};
+struct is_linear_container<std::vector<Ts...>> : std::true_type
+{
+};
 
 } // namespace mbgl
